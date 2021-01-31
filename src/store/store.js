8448082edffa1f966c35476100852528b414783b
Mko <mstepanyan@technolonguistics.org>
2021-01-31 13:07:41 +0400

    Fixed logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import loggerMiddleware from "redux-logger";
+import thunk from "redux-thunk";
 import thunkMiddleware from "redux-thunk";
-import {mainReducer} from "./reducer";
+import { mainReducer } from "./reducer";
 
-const middlewares = applyMiddleware( thunkMiddleware,loggerMiddleware);
+const middlewaresArray = [thunk];
+if (process.env.NODE_ENV === "development") {
+    middlewaresArray.push(loggerMiddleware)
+}
+
+const middlewares = applyMiddleware(...middlewaresArray);
 
 export const store = createStore(mainReducer, middlewares);
